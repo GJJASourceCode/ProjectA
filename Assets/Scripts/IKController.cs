d03b3c466d6e0e0e3bc01045765adefe08fa535d
deopoler <50035578+Deopoler@users.noreply.github.com>
2022-12-13 21:56:24 +0900

    Change Animation and Arm IK

--- Assets/Scripts/IKController.cs
+++ Assets/Scripts/IKController.cs
@@ -6,6 +6,7 @@ public class IKController : MonoBehaviour
 {
     private Animator animator;
     [SerializeField] private bool ikActive;
+    [SerializeField] private Transform handPos;
     [SerializeField] private Transform lookAtPos;
     void Start()
     {
@@ -20,11 +21,17 @@ public class IKController : MonoBehaviour
             {
                 animator.SetLookAtWeight(1);
                 animator.SetLookAtPosition(lookAtPos.position);
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0.7f);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0.7f);
+                animator.SetIKPosition(AvatarIKGoal.RightHand, handPos.position);
+                animator.SetIKRotation(AvatarIKGoal.RightHand, handPos.rotation);
             }
         }
         else
         {
             animator.SetLookAtWeight(0);
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
         }
     }
 }
